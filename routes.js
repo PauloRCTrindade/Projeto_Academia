e538593882da86dde7862de205205e5f4e5b80bd
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-06-19 12:20:20 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -2,11 +2,19 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/',function(req,res){
-    return res.redirect('/Instrutores')
+    return res.redirect('/instrutores')
 })
 
 routes.get('/instrutores',function(req,res){
     return res.render('instrutores/index')
+})
+
+routes.get('/instrutores/create',function(req,res){
+    return res.render('instrutores/create')
+})
+
+routes.post("/instrutores",function(req,res){
+    return res.send('instrutores')
 })
 
 routes.get('/membros',function(req,res){
