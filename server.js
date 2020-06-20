60aab19370441ff2472d6af1264891a46684b540
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-06-20 11:35:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const routes = require('./routes')
 
 const server = express()
 
+server.use(express.urlencoded({extended: true}))
 server.use('/public',express.static('public'))
 server.use(routes)
 
