d442c67be714298229108eb06d2c7b0e220cbd2e
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-06-22 17:54:57 -0300

    Update instrutores.js

--- instrutores.js
+++ instrutores.js
@@ -10,7 +10,28 @@ exports.show = function(req,res){
 
     if (!foundInstrutores) return res.send("Instrutor não encontrado")
 
-    return res.render("instrutores/show",{instrutores: foundInstrutores})
+    function age(timestamp){
+        const today = new Date()
+        const birthDate = new Date(timestamp)
+
+        let age = today.getFullYear() - birthDate.getFullYear()
+        const month = today.getMonth() - birthDate.getMonth()
+
+        if (month < 0 || month == 0 && today.getDate() <= birthDate.getDate()){
+            age = age - 1
+        }
+
+        return age
+    }
+
+    const instrutores = {
+        ... foundInstrutores,
+        idade: age(foundInstrutores.idade),
+        funcao: foundInstrutores.funcao.split(","),
+        data_inicio:"",
+    }
+
+    return res.render("instrutores/show",{instrutores})
 }
 
 exports.post =  function(req,res){
