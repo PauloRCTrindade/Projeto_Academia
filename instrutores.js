8d82c02599bf2313d5612d4085f26b0337fabf2d
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-06-24 11:33:48 -0300

    Update instrutores.js

--- instrutores.js
+++ instrutores.js
@@ -2,6 +2,12 @@ const fs = require('fs')
 const data = require('./data.json')
 const { age, date } = require('./utils')
 
+
+//index
+exports.index = function(req,res){
+    return res.render('instrutores/index',{instrutores: data.instrutores})
+}
+
 // Show
 exports.show = function(req,res){
     const {id} = req.params
